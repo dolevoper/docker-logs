48643672d005fc4067b588aae4b13b010e362ec4
Omer Dolev <omerdolev90@gmail.com>
2020-05-23 12:10:51 +0300

    add docker-compose

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+const { Docker } = require('node-docker-api');
+
+const docker = new Docker({ socketPath: '/var/run/docker.sock' });
+
+async function app() {
+    const images = await docker.image.list();
+
+    images.forEach(image => console.log(image.id));
+}
+
+app();
